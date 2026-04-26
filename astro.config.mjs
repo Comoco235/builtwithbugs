daefca00572e6cea5ad7207a064bd5f7aef4c6ce
Comoco235 <samwaneab@gmail.com>
2026-04-27 00:19:35 +0200

    feat : amelioration snippet + SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,23 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://builtwithbugs.com',
-	integrations: [mdx(), sitemap()],
-	prefetch: {
-		prefetchAll: true,
-		defaultStrategy: 'hover'
-	},
+  site: 'https://www.builtwithbugs.com',
+  integrations: [mdx(), sitemap()],
+
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: 'viewport'
+  },
+
+  adapter: vercel({
+    imageService: true,
+    imagesConfig: {
+      sizes: [320, 640, 1280],
+      domains: [],
+    }
+  }),
 });
